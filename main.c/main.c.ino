29e5da5cbb1718ae7ec2503b97c0d2a249430868
unknown <pauloffxii@hotmail.com>
2022-05-05 20:00:34 -0300

    init commit

---
+++ main.c/main.c.ino
@@ -0,0 +1,19 @@
+/* Programa da VMG para Oficinas de Integração 2 */
+#include "app.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+  vAPP_Init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  while(1)
+  { 
+    vAPP_Poll();
+    
+  }
+
+}
